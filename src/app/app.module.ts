0ae1b956bf3f47dc2cbf7c780f107b6be277f914
Gerson Ribeiro <gerson.mano@gmail.com>
2020-04-06 12:01:50 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JadeIntegrationUtilsModule } from 'projects/jade-integration-utils/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JadeIntegrationUtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
